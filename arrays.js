f60d1407802c47199b68c47991f36a1da87dfd2d
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-02-07 17:12:49 +0500

    hi

---
+++ arrays.js
@@ -0,0 +1,3 @@
+const arr = [1,2,3,4,5,6,78,9,0]
+
+module.exports={arr}
---
+++ index.js
@@ -0,0 +1,30 @@
+// setInterval(()=>{
+//     console.log('2 sekund otdi.');
+// },2000)
+
+
+// const nArr = ['1','8','5']
+
+// function findMax(n){
+//     let max = 0;
+//     for(let i = 0; i<n.length;i++){
+//         if(n[i]> max){
+//             max = n[i]
+//         }
+//     }
+//     return max
+// }
+
+// console.log(findMax(nArr));
+
+// const arr1 = ['1','3','4']
+// const arr2 = ['2','3','5']
+
+// function ArrNums(a,b){
+//     for(var i = 0; i<b.length; i++){
+//         if(!a.includes(b[i])) a.push(b[i])
+//     }
+//     return a
+// }
+// console.log(ArrNums(arr1, arr2));
+
